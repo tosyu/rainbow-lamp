a948a0bfa22fed8d064fdb03a6caf2e690979317
Krzysztof Antoszek <k.antoszek@samsung.com>
2013-05-13 16:11:23 +0200

    removed time.h lib in favor of incremental seed

--- rainbow_lamp.c
+++ rainbow_lamp.c
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #endif
 #include <stdlib.h>
-#include <time.h>
 
 #define LED_NUMBER 3
 #define uint8_t unsigned char
@@ -17,44 +16,50 @@ typedef struct {
 } color;
 
 color led[LED_NUMBER];
+uint8_t rseed = 0;
+uint8_t max_rand = sizeof(uint8_t) * 255;
 
-void apply_random_color(color * c);
-void reset_leds(color * leds, uint8_t length);
+void apply_random_color(color *c);
+void reset_leds(color *leds, uint8_t length);
+void reseed(void);
 
 int main()
 {
-    int i, seed;
-
-    /* seed the random number generator */
-    seed = time(NULL);
-    srand(seed);
+    int i;
 
     /* initialize leds */
     reset_leds(led, LED_NUMBER);
 
 #ifdef __unix
     for (i = 0; i < LED_NUMBER; ++i) {
-	printf("#%d R:%d G:%d B:%d\n", (i + 1), led[i].r, led[i].g,
-	       led[i].b);
+	  printf("#%d R:%d G:%d B:%d\n", (i + 1), led[i].r, led[i].g, led[i].b);
     }
 #endif
 
     return 0;
 }
 
-void reset_leds(color * leds, uint8_t length)
+void reset_leds(color *leds, uint8_t length)
 {
     int i;
     for (i = 0; i < length; ++i) {
-	apply_random_color(&leds[i]);
+	  apply_random_color(&leds[i]);
     }
 }
 
-void apply_random_color(color * c_ptr)
+void apply_random_color(color *c_ptr)
 {
-    uint8_t max_rand = sizeof(uint8_t) * 255;
-
+    reseed();
     c_ptr->r = (uint8_t) (rand() / (RAND_MAX / 1.0) * max_rand);
     c_ptr->g = (uint8_t) (rand() / (RAND_MAX / 1.0) * max_rand);
     c_ptr->b = (uint8_t) (rand() / (RAND_MAX / 1.0) * max_rand);
+}
+
+void reseed() {
+  if (rseed == 255) {
+	rseed = 0;
+  } else {
+	++rseed;
+  }
+  srand(rseed);
 }
